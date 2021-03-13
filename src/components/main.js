4171075e07e6405c75fdc6c118745864439e644c
David Trussler <mail@davidtrussler.net>
2021-03-13 16:08:32 +0000

    Updates `main` component

--- src/components/main.js
+++ src/components/main.js
@@ -2,7 +2,7 @@ const AppMain = {
 	props: ['pageTitle'], 
 	template: `
 		<main>
-			<p>This is the main section of the {{ pageTitle }} page</p>
+			<p>This is the main section of the site</p>
 		</main>
 	`	
 }
