0d0ed57a700150d6549e6cf68ad13c92e63854a6
David Trussler <mail@davidtrussler.net>
2021-03-13 16:10:23 +0000

    Creates markup for basic structure (VueJS)     - Installs Vue     - Installs Vue server-renderer     - Adds `app` component     - Adds common header component     - Adds common footer component     - Adds main component

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const Vue = require('vue'); 
+const AppHeader = require('./components/header'); 
+const AppMain = require('./components/main'); 
+const AppFooter = require('./components/footer'); 
+
+module.exports = function createSSRApp(url) {
+	return Vue.createSSRApp({
+		components: {
+			'app-header': AppHeader, 
+			'app-main': AppMain, 
+			'app-footer': AppFooter
+		}, 
+		template: `
+			<!DOCTYPE html>
+			<html lang="en-GB">
+				<head>
+					<title>Movie Database Search</title>
+				</head>
+
+				<body>
+					<app-header></app-header>
+					<app-main></app-main>
+					<app-footer></app-footer>
+				</body>
+			</html>
+		`
+	})
+}
---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+const AppMain = {
+	props: ['pageTitle'], 
+	template: `
+		<main>
+			<p>This is the main section of the site</p>
+		</main>
+	`	
+}
+
+module.exports = AppMain;
