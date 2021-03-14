ef705af4b3f7bccf8be40d18eb9ea562dd0d821b
David Trussler <mail@davidtrussler.net>
2021-03-14 21:56:41 +0000

    Updates styles for initial header

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,15 @@
 const AppHeader = {
 	template: `
 		<header>
-			<h1>MMTFlix</h1>
-			<form>
+			<h1 class="header__title">
+				<span class="visually-hidden">MMTFlix</span>
+			</h1>
+
+			<form name="search">
 				<input name="search" type="text" placeholder="Search here ..."/>
-				<button type="submit" name="submit">Search</button>
+				<button type="submit" name="submit">
+					<span class="visually-hidden">Search</span>
+				</button>
 			</form>
 		</header>
 	`	
