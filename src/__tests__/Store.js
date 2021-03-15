f78dfd32d2f8fbf5562067c2a24f4a5ea34a7254
David Trussler <mail@davidtrussler.net>
2021-03-15 23:00:12 +0000

    Adds test scenario for pagination (WIP)

--- src/__tests__/Store.js
+++ src/__tests__/Store.js
@@ -1,6 +1,9 @@
 const Store = require('../client/Store');
 const mockData = require('../../db.json');
 
+// GIVEN I am on the search page
+// WHEN I have entered a search term
+// THEN I can see a grid of movies, each showing the poster, title and year of release
 describe('search', () => {
 	let sut;
 
@@ -25,3 +28,20 @@ describe('search', () => {
 		expect(result).toEqual(expected);
 	});
 });
+
+// GIVEN I am on the search page
+// WHEN the search results are displayed
+// THEN I can see the results load, (showing x of x) to indicate the current paginated group being displayed
+// AND I can click arrows to navigate between the groups of results
+describe('pagination', () => {
+	let sut;
+	let result;
+
+	beforeEach(() => {
+		// Arrange
+		sut = new Store();
+		sut.state.films = mockData.films;
+	}); 
+
+	it()
+}); 
