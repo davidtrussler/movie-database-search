d6b691a9b3ccaca9aacde821b53c0fe6c7e2f4eb
David Trussler <mail@davidtrussler.net>
2021-03-17 17:40:14 +0000

    Adds new `modal` component

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@ const Vue = require('vue');
 const AppHeader = require('./components/header'); 
 const AppMain = require('./components/main'); 
 const AppFooter = require('./components/footer'); 
+const AppModal = require('./components/modal');
 
 module.exports = function createSSRApp(url) {
 	return Vue.createSSRApp({
 		components: {
 			'app-header': AppHeader, 
 			'app-main': AppMain, 
-			'app-footer': AppFooter
+			'app-footer': AppFooter,
+			'app-modal': AppModal
 		}, 
 		template: `
 			<!DOCTYPE html>
@@ -22,6 +24,7 @@ module.exports = function createSSRApp(url) {
 					<app-header></app-header>
 					<app-main></app-main>
 					<app-footer></app-footer>
+					<app-modal></app-modal>
 				</body>
 			</html>
 		`
---
+++ src/components/modal.js
@@ -0,0 +1,8 @@
+const AppModal = {
+	template: `
+		<div data-results-modal></div>
+		<div data-results-modal-overlay></div>
+	`	
+}
+
+module.exports = AppModal;
